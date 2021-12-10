2f1c63d846a4f8509e37f95ca91476468679dfa3
EllaKCollins <49648602+EllaKCollins@users.noreply.github.com>
2021-12-10 10:36:21 +0100

    plots done

--- Assignment1/assignment1.m
+++ Assignment1/assignment1.m
@@ -1,41 +1,56 @@
 n_D = 50;
 alphas = [0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3.0];
-success_rates = zeros(length(alphas), 1);
-for x = 1:length(alphas)
-    alpha = 0.75; % alphas(x);
-    succesfull_runs = 0;
-    for n = 1:n_D   
-        epochs = 100; % number of epochs
-        N = 10; % dimension of feature vectors - use 20 and 40 at least
-        P = cast(N*alpha, 'uint8'); % number of sets
-        
-        % we create P randomly-generated N-dimensional vectors
-        y = randn(P, N);
 
-        % we generate P random numbers with values either -1 or 1
-        S = 2*(rand(1,P)>0.5) - 1;
+epochs = 100; % number of epochs
+Ns = [20, 40, 60, 80, 100]; % array of dimension of feature vectors - use 20 and 40 at least
+N_success = zeros(length(Ns),length(alphas));
+for N = 1:length(Ns)
+    success_rates = zeros(length(alphas), 1);
+    for x = 1:length(alphas)
+        alpha = alphas(x);
+        succesfull_runs = 0;
+        for n = 1:n_D   
+            P = cast(N*alpha, 'uint8'); % number of sets
 
-        % intialize weights at 0
-        w = zeros(N, 1);
-        E = zeros(P, 1);
+            % we create P randomly-generated N-dimensional vectors
+            y = randn(P, N);
 
-        % double for-loop for the sequential perceptron training
-        for epoch = 1:epochs
-            for example = 1:P % should probs rename this
-                current_w = w;
-                E(example) = dot(current_w,(y(example,:).*S(example)));
-                if E <= 0
-                    w = current_w + (y(example,:).*S(example))'/N ;
-                end % dont need an else because the value doesn't change
-            end
-            %fprintf("E = %d\n", E);
-            if all(E>0)
-                %fprintf("success\n");
-                succesfull_runs = succesfull_runs + 1; 
-                break;
+            % we generate P random numbers with values either -1 or 1
+            S = 2*(rand(1,P)>0.5) - 1;
+
+            % intialize weights at 0
+            w = zeros(N, 1);
+            E = zeros(P, 1);
+            % double for-loop for the sequential perceptron training
+            for epoch = 1:epochs
+                for example = 1:P % should probs rename this
+                    current_w = w;
+                    E(example) = dot(current_w,(y(example,:).*S(example)));
+                    if E(example) <= 0
+                        w = current_w + (y(example,:).*S(example))'/N ;
+                    end % dont need an else because the value doesn't change
+                end
+                if all((E>0))
+                    succesfull_runs = succesfull_runs + 1; 
+                    break;
+                end
             end
         end
+        success_rates(x) = succesfull_runs / n_D ;
     end
-    success_rates(x) = succesfull_runs / n_D ;
-    fprintf("rate: %d\n", success_rates(x));
-end 
+    N_success(N,:) = success_rates;
+end
+
+% plotting the results
+figure;
+plot(alphas, N_success(1,:));
+title('$Q_{l.s.}$ as a function of $\alpha$', 'Interpreter', 'latex');
+xlabel("$\alpha = P/N$",'Interpreter', 'latex');
+ylabel("$Q_{l.s.}$", 'Interpreter', 'latex');
+hold on;
+for p = 2:length(Ns)
+    plot(alphas, N_success(p,:));
+end
+hold off;
+lengendCell = cellstr(num2str(Ns', 'N=%-d'));
+legend(lengendCell);
