3ce7d48c99ff3b771df214f19e95e999ac2ece09
Andrei Stoica <andreistoica12@gmail.com>
2022-02-04 20:11:36 +0100

    fixed some logic, but still something wrong

--- Assignment2/assignment2.m
+++ Assignment2/assignment2.m
@@ -1,10 +1,10 @@
 n_D = 50; % number of runs
 
-% alphas = [0.25, 0.5, 1, 3.0, 5.0]
+% alphas = [0.25, 0.5, 1, 3.0, 5.0];
 alphas = [0.25, 0.5, 1.0, 1.5, 2.0, 3.0, 4.0, 5.0]; % alphas as in assignment
 % alphas = [0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3.0];
 
-P_vals = [5, 20];
+P_vals = [5, 20, 35];
 % P_vals = [5, 20, 40, 60, 80, 100]; % change to P values ?
 N_gen_errors = zeros(length(P_vals),length(alphas)); % should be for number of Ps
 n_max = 100;
@@ -14,6 +14,7 @@ for p_value = 1:length(P_vals)
     t_max = n_max * P;  % number of epochs
     gen_errors = zeros(1, length(alphas));
     t_max_final = zeros(1, length(alphas));
+    t_max_final(:) = t_max;
     for x = 1:length(alphas)
         alpha = alphas(x);
         gen_error = 0;
@@ -50,9 +51,13 @@ for p_value = 1:length(P_vals)
             k = zeros(P, 1);
             E = zeros(P, 1);
             
-            for epoch = 1:t_max
+            for epoch = 0:t_max - 1
                 for example = 1:P
-                    current_w = w(:, t_max);
+                    if epoch == 0
+                        current_w = zeros(N, 1);
+                    else
+                        current_w = w(:, epoch);
+                    end
                     E(example) = (y(example, :) * current_w) * S(example);
                     if norm(current_w) ~= 0 % ensure no division by 0
                         k(example) = E(example) / norm(current_w);
@@ -64,24 +69,26 @@ for p_value = 1:length(P_vals)
                 end
                 % we determine the example with lowest stability
                 [min_example, min_index] = min(k); % find(k == min(k)) 
-                w(:, epoch) = current_w + (y(example,:)' * S(example)) ./ double(N) ;  % update the weights
+                w(:, epoch + 1) = current_w + (y(example,:)' * S(example)) ./ double(N) ;  % update the weights
                 
                 % TODO: determine a good stopping criterion
                 if epoch > P
-                    angular_change = acos(dot(w(:, epoch - P), w(:, epoch)) / ...
-                        norm(w(:, epoch - P)) * norm(w(:, epoch))) / pi;
-%                     angular_change = acos(dot(w((epoch-P),:), w(epoch, :)) / ...
-%                         (norm(w((epoch-P),:))*norm(w(epoch, :)))) / pi ;
+                    % TODO: solve this argument (it seems to be always 1)
+                    argument = dot(w(:, epoch - P), w(:, epoch)) / ...
+                        (norm(w(:, epoch - P)) * norm(w(:, epoch)));
+%                     fprintf("argument = %d", argument);
+                    angular_change = acos(argument) / pi ;
+                    fprintf("angular change = %d \n", angular_change);
+
+
                     if angular_change < 0.1
-                        gen_error = gen_error + acos(dot(w(:, epoch), w_star) / ...
-                            (norm(w(:, epoch)) * norm(w_star))) / pi ;
                         t_max_final(1, x) = epoch ;
                         break;
-                    else
-                        t_max_final(1, x) = t_max;
                     end
                 end
             end
+            gen_error = gen_error + acos(dot(w(:, t_max), w_star) / ...
+                (norm(w(:, t_max)) * norm(w_star))) / pi ;
         end
         gen_errors(1, x) = gen_error / n_D ; 
     end
@@ -90,18 +97,18 @@ end
 
 % TODO: plot
 % plotting the results
-% figure;
-% plot(alphas, N_gen_errors(1,:), '-*');
-% title('$\epsilon_{g}$ as a function of $\alpha$', 'Interpreter', 'latex');
-% xlabel("$\alpha = P/N$",'Interpreter', 'latex');
-% ylabel("$\epsilon_{g}$", 'Interpreter', 'latex');
+figure;
+plot(alphas, mean(N_gen_errors), '-*');
+title('$\epsilon_{g}$ as a function of $\alpha$', 'Interpreter', 'latex');
+xlabel("$\alpha = P/N$",'Interpreter', 'latex');
+ylabel("$\epsilon_{g}$", 'Interpreter', 'latex');
 % hold on;
 % for p = 2:size(N_gen_errors, 1)
 %     plot(alphas, N_gen_errors(p,:), '-*');
 % end
 % hold off;
-% lengendCell = cellstr(num2str(N_gen_errors(:,1)', 'N=%-d'));
-% legend(lengendCell);
+legendCell = cellstr(num2str(mean(N_gen_errors)', 'N=%-d'));
+legend(legendCell);
 
 % TODO: test 
 % TODO: iris flower data (extension)
